78d9f98c4e30a02c6dddb908bbedb16c898a3915
ashutoshlovpry <67589741+ashutoshlovpry@users.noreply.github.com>
2022-03-27 10:51:44 +0530

    Update settings.py

--- EmailProject/settings.py
+++ EmailProject/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.yahoo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'priyyad.com'
+EMAIL_HOST_USER = 'priyyad@yahoo.com'
 EMAIL_HOST_PASSWORD = 'ozetrvbizsydngaz' #'iokhsaoeoiopryxl'
